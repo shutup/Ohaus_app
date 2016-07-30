1b5b3060d19354c61a0a15d93173e0b29fcb6d32
shutup <fyjc999@gmail.com>
2016-07-30 11:10:26 +0800

    implements user info ui like wechat

--- app/src/main/java/com/shutup/ohaus_app/me/MeActivity.java
+++ app/src/main/java/com/shutup/ohaus_app/me/MeActivity.java
@@ -2,28 +2,60 @@ package com.shutup.ohaus_app.me;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.shutup.ohaus_app.BuildConfig;
 import com.shutup.ohaus_app.R;
 import com.shutup.ohaus_app.common.BaseActivity;
 
-import java.util.ArrayList;
-
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import butterknife.OnClick;
 
-public class MeActivity extends BaseActivity implements MeConstatnts{
+public class MeActivity extends BaseActivity implements MeConstatnts {
 
     @InjectView(R.id.toolbar_title)
     TextView mToolbarTitle;
     @InjectView(R.id.toolbar)
     Toolbar mToolbar;
-    @InjectView(R.id.userInfoListView)
-    ListView mUserInfoListView;
+    @InjectView(R.id.me_info_photo)
+    ImageView mMeInfoPhoto;
+    @InjectView(R.id.me_info_photo_layout)
+    RelativeLayout mMeInfoPhotoLayout;
+    @InjectView(R.id.me_info_name)
+    TextView mMeInfoName;
+    @InjectView(R.id.me_info_name_layout)
+    RelativeLayout mMeInfoNameLayout;
+    @InjectView(R.id.me_info_phone)
+    TextView mMeInfoPhone;
+    @InjectView(R.id.me_info_phone_layout)
+    RelativeLayout mMeInfoPhoneLayout;
+    @InjectView(R.id.me_info_mail)
+    TextView mMeInfoMail;
+    @InjectView(R.id.me_info_mail_layout)
+    RelativeLayout mMeInfoMailLayout;
+    @InjectView(R.id.me_info_job)
+    TextView mMeInfoJob;
+    @InjectView(R.id.me_info_job_layout)
+    RelativeLayout mMeInfoJobLayout;
+    @InjectView(R.id.me_info_company)
+    TextView mMeInfoCompany;
+    @InjectView(R.id.me_info_company_layout)
+    RelativeLayout mMeInfoCompanyLayout;
+    @InjectView(R.id.me_info_province)
+    TextView mMeInfoProvince;
+    @InjectView(R.id.me_info_province_layout)
+    RelativeLayout mMeInfoProvinceLayout;
+    @InjectView(R.id.me_info_city)
+    TextView mMeInfoCity;
+    @InjectView(R.id.me_info_city_layout)
+    RelativeLayout mMeInfoCityLayout;
 
-    private ArrayList<MeInfoItem> mMeInfoItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,29 +63,16 @@ public class MeActivity extends BaseActivity implements MeConstatnts{
         setContentView(R.layout.activity_me);
         ButterKnife.inject(this);
         initToolBar();
-        initData();
-    }
-
-    private void initData() {
-        mMeInfoItems = new ArrayList<>();
-        mMeInfoItems.add(new MeInfoItem("姓名","", TYPE_1));
-        mMeInfoItems.add(new MeInfoItem("手机","", TYPE_1));
-        mMeInfoItems.add(new MeInfoItem("邮箱","", TYPE_1));
-        mMeInfoItems.add(new MeInfoItem("职务","", TYPE_1));
-        mMeInfoItems.add(new MeInfoItem("公司","", TYPE_1));
-        mMeInfoItems.add(new MeInfoItem("省份","", TYPE_2));
-        mMeInfoItems.add(new MeInfoItem("城市","", TYPE_2));
-
-        mUserInfoListView.setAdapter(new MeInfoListAdapter(mMeInfoItems,this));
 
     }
+
 
     private void initToolBar() {
         if (mToolbar != null) {
             mToolbar.setNavigationIcon(R.drawable.back_arrow_white);
             setSupportActionBar(mToolbar);
         }
-        if (getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setTitle("");
             mToolbarTitle.setText(R.string.menu_me_title);
         }
@@ -67,5 +86,26 @@ public class MeActivity extends BaseActivity implements MeConstatnts{
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick({R.id.me_info_photo_layout, R.id.me_info_name_layout, R.id.me_info_phone_layout, R.id.me_info_mail_layout, R.id.me_info_job_layout, R.id.me_info_company_layout, R.id.me_info_province_layout})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.me_info_photo_layout:
+            if (BuildConfig.DEBUG) Log.d("MeActivity", "clicked");
+                break;
+            case R.id.me_info_name_layout:
+                break;
+            case R.id.me_info_phone_layout:
+                break;
+            case R.id.me_info_mail_layout:
+                break;
+            case R.id.me_info_job_layout:
+                break;
+            case R.id.me_info_company_layout:
+                break;
+            case R.id.me_info_province_layout:
+                break;
+        }
     }
 }
