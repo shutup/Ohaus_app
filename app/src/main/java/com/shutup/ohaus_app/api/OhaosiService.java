a8e3ecf92e9e66d7d751d797ee83d1624580fd4e
shutup <fyjc999@gmail.com>
2016-09-26 18:44:28 +0800

    merge code

--- app/src/main/java/com/shutup/ohaus_app/api/OhaosiService.java
+++ app/src/main/java/com/shutup/ohaus_app/api/OhaosiService.java
@@ -2,8 +2,14 @@ package com.shutup.ohaus_app.api;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+
 
 /**
  * Created by shutup on 16/9/22.
@@ -12,4 +18,9 @@ import retrofit2.http.GET;
 public interface OhaosiService {
     @GET("api/category/all")
     Call<List<CategoryEntity>> getAllCategory();
+
+
+    @FormUrlEncoded
+    @POST("api/users/login")
+    Call<ResponseBody> userLogin(@Field("phone") String phone, @Field("password") String password);
 }
