9d7625af52e05a64c2ad54bba81e92868e853d39
shutup <fyjc999@gmail.com>
2016-09-28 23:20:11 +0800

    temp save

--- app/src/main/java/com/shutup/ohaus_app/api/OhaosiService.java
+++ app/src/main/java/com/shutup/ohaus_app/api/OhaosiService.java
@@ -8,7 +8,6 @@ import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 
