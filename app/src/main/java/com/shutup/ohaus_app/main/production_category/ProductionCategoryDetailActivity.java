885eff36f4e91aefdc0e39cb15242852342c4ead
shutup <fyjc999@gmail.com>
2016-10-26 22:30:41 +0800

    add detail filter layout

--- app/src/main/java/com/shutup/ohaus_app/main/production_category/ProductionCategoryDetailActivity.java
+++ app/src/main/java/com/shutup/ohaus_app/main/production_category/ProductionCategoryDetailActivity.java
@@ -1,17 +1,24 @@
 package com.shutup.ohaus_app.main.production_category;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.shutup.ohaus_app.R;
+import com.shutup.ohaus_app.api.ProductCategoryEntity;
 import com.shutup.ohaus_app.common.BaseActivity;
 import com.shutup.ohaus_app.model.ProductionNormalItem;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -28,6 +35,28 @@ public class ProductionCategoryDetailActivity extends BaseActivity {
     View mProductionCategoryDetailToolbarLineLeft;
     @InjectView(R.id.production_category_detail_toolbar_line_right)
     View mProductionCategoryDetailToolbarLineRight;
+    @InjectView(R.id.production_category_filter_option_view_price_label)
+    TextView mProductionCategoryFilterOptionViewPriceLabel;
+    @InjectView(R.id.production_category_filter_option_view_model_label)
+    TextView mProductionCategoryFilterOptionViewModelLabel;
+    @InjectView(R.id.production_category_filter_option_view_order_label)
+    TextView mProductionCategoryFilterOptionViewOrderLabel;
+    @InjectView(R.id.production_category_filter_option_top_bar)
+    RelativeLayout mProductionCategoryFilterOptionTopBar;
+    @InjectView(R.id.production_category_filter_option_view_btn_ok)
+    Button mProductionCategoryFilterOptionViewBtnOk;
+    @InjectView(R.id.recyclerViewFilterOptionsDetails)
+    RecyclerView mRecyclerViewFilterOptionsDetails;
+    @InjectView(R.id.production_category_filter_option_view_item_icon)
+    ImageView mProductionCategoryFilterOptionViewItemIcon;
+    @InjectView(R.id.production_detail_name_price_title)
+    TextView mProductionDetailNamePriceTitle;
+    @InjectView(R.id.production_detail_name_price_options)
+    Button mProductionDetailNamePriceOptions;
+
+    private ProductCategoryEntity mProductCategoryEntity;
+    private ArrayList<FilterOptionModel> mFilterOptionModels;
+    private ProductionNormalItem mProductionNormalItem;
 
     @Override
     public void onStart() {
